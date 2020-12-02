37da0fc7b9ce585cd578bcde0cc61567a104a67d
Shaojin Ding <shjd@tamu.edu>
2020-12-01 22:06:09 -0600

    Update preprocess.py
    Fix process_speaker to avoid the need of alignment

--- synthesizer/preprocess.py
+++ synthesizer/preprocess.py
@@ -148,30 +148,15 @@ def preprocess_librispeech(datasets_root: Path, out_dir: Path, n_processes: int,
 
 def preprocess_speaker(speaker_dir, out_dir: Path, skip_existing: bool, hparams):
     metadata = []
-    for book_dir in speaker_dir.glob("*"):
-        # Gather the utterance audios and texts
-        try:
-            alignments_fpath = next(book_dir.glob("*.alignment.txt"))
-            with alignments_fpath.open("r") as alignments_file:
-                alignments = [line.rstrip().split(" ") for line in alignments_file]
-        except StopIteration:
-            # A few alignment files will be missing
-            continue
+    for wav_fpath in speaker_dir.glob("*"):
+        assert wav_fpath.exists()
+        wav, _ = librosa.load(wav_fpath, hparams.sample_rate)
+        if hparams.rescale:
+            wav = wav / np.abs(wav).max() * hparams.rescaling_max
+        wav, _ = librosa.effects.trim(wav, top_db=25)
+        wav_cat_fname = '{}-{}'.format(speaker_dir.name, wav_fpath.stem)
 
-        # Iterate over each entry in the alignments file
-        for wav_fname, words, end_times in alignments:
-            wav_fpath = book_dir.joinpath(wav_fname + ".flac")
-            assert wav_fpath.exists()
-            words = words.replace("\"", "").split(",")
-            end_times = list(map(float, end_times.replace("\"", "").split(",")))
-
-            # Process each sub-utterance
-            wavs, texts = split_on_silences(wav_fpath, words, end_times, hparams)
-            for i, (wav, text) in enumerate(zip(wavs, texts)):
-                sub_basename = "%s_%02d" % (wav_fname, i)
-                metadata.append(process_utterance(wav, text, out_dir, sub_basename,
-                                                  skip_existing, hparams))
-
+        metadata.append(process_utterance(wav, out_dir, wav_cat_fname, skip_existing, hparams))
     return [m for m in metadata if m is not None]
 
 
